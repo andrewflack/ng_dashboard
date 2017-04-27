54e2e265897b29754b45c40bbfc6a7e4a9127d3c
Andrew Flack <flack.andrew@gmail.com>
2017-04-26 21:01:27 -0400

    initial commit

---
+++ NG_dashboard.R
@@ -0,0 +1,59 @@
+# Define UI
+ui <- dashboardPage(
+  
+  # Application title 
+  dashboardHeader(title = "NetGalley Dashboard"),
+  
+  # Sidebar
+  dashboardSidebar(    
+    sidebarMenu(
+      menuItem("Tab 1", 
+               tabName = "tab1", 
+               icon = icon("bar-chart"))
+    )
+  ),
+  
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "tab1",
+        fluidRow(
+          box(
+            title = "plot 1",
+            solidHeader = TRUE,
+            plotlyOutput("plot1")
+          )
+        ),
+        fluidRow(
+          box(
+            title = "plot2",
+            solidHeader = TRUE
+          )
+        )
+      )
+    )
+  )
+)
+
+# Define server logic required
+server <- function(input, output) {
+   
+   output$plot1 <- renderPlotly({
+     p <- counts %>% 
+       ggplot(aes(y = median_amazon_reviews_on_ng,
+                  x = median_amazon_reviews_off_ng, 
+                  size = n_contracted_titles,
+                  text = paste0("Publisher: ", billing_name))) +
+       geom_jitter(alpha = .5) +
+       geom_abline(aes(intercept = 0, slope = 1), colour = "red") +
+       theme_minimal() +
+       labs(y = "Median Amz Reviews On NG",
+            x = "Median Amz Reviews Off NG") +
+       theme(legend.position = "none")
+     
+     ggplotly(p)
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
---
+++ utils.R
@@ -0,0 +1,20 @@
+# NG_dashboard utils
+
+library(shiny)
+library(shinydashboard)
+library(ggplot2)
+library(plotly)
+library(dplyr)
+library(RSQLite)
+
+# open connection to database
+con <- dbConnect(SQLite(), dbname = "dashboard_data/NewReviewStats.db")
+
+# pull out publisherCounts
+counts <- dbGetQuery(con, 'select * from publisherCounts' )
+
+counts <- counts %>% 
+  select(-index, -account_id, -primary_market)
+
+counts$median_amazon_reviews_on_ng[which(is.na(counts$median_amazon_reviews_on_ng))] <- 0
+counts$title_count_on_ng[which(is.na(counts$title_count_on_ng))] <- 0
